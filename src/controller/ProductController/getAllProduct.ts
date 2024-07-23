5bd782b9b53dd41ce9d24de18989bb323b9409a2
repirisyan <repirisyan@icloud.com>
2024-07-23 16:25:15 +0700

    add filter

--- src/controller/ProductController/getAllProduct.ts
+++ src/controller/ProductController/getAllProduct.ts
@@ -4,14 +4,34 @@ import product from "../../models/product/getAllProduct"; // Import the model
 export const getAllProduct = async (req: Request, res: Response) => {
   const page = parseInt(req.query.page as string, 10) || 1; // Default page to 1 if not specified
   const limit = parseInt(req.query.limit as string, 10) || 10; // Default limit to 10 if not specified
-
+  const search = (req.query.search as string) || null;
+  const marketplace = req.query.marketplace as string || null
+  const comodity = req.query.comodity as string || null
   try {
     const options = {
       page,
       limit,
     };
+    let searchQuery: any = {};
+    if (search != null) {
+      searchQuery.$or = [
+        { comodity: { $regex: search, $options: "i" } },
+        { location: { $regex: search, $options: "i" } },
+        { marketplace: { $regex: search, $options: "i" } },
+        { seller: { $regex: search, $options: "i" } },
+        { title: { $regex: search, $options: "i" } },
+      ];
+    }
 
-    const result = await product.paginate({}, options);
+    if (marketplace != null) {
+      searchQuery.marketplace = marketplace;
+    }
+
+    if (comodity != null) {
+      searchQuery.comodity = comodity;
+    }
+
+    const result = await product.paginate(searchQuery, options);
 
     res.json(result);
   } catch (error) {
--- src/index.ts
+++ src/index.ts
@@ -1,9 +1,24 @@
 import express, { Express, Request, Response } from "express";
+import fs from "fs";
+import https from "https";
+import path from "path";
+import cors from "cors";
 import { connectDB } from "./db/connection"; // Import the connectDB function
 import { getAllProduct } from "./controller/ProductController/getAllProduct";
 
 const app: Express = express();
 const port = 3030;
+
+app.use(cors());
+
+// Read SSL certificate and key
+const key = fs.readFileSync(path.join(__dirname, "../key.pem"));
+const cert = fs.readFileSync(path.join(__dirname, "../cert.pem"));
+
+const options = {
+  key,
+  cert,
+};
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -20,5 +35,9 @@ connectDB().catch((error: any) => {
 app.get("/product", getAllProduct);
 
 app.listen(port, () => {
-  console.log(`[server]: Server is running at ${port}`);
+  console.log(`Server is running on http://localhost:${port}`);
 });
+
+// https.createServer(options, app).listen(port, () => {
+//   console.log(`Server is running on https://localhost:${port}`);
+// });
