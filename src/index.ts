bc839372044541a25bc6c01a9326e1f9f4565497
repirisyan <repirisyan@icloud.com>
2024-07-23 16:26:30 +0700

    fix API

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,4 @@
 import express, { Express, Request, Response } from "express";
-import fs from "fs";
-import https from "https";
-import path from "path";
 import cors from "cors";
 import { connectDB } from "./db/connection"; // Import the connectDB function
 import { getAllProduct } from "./controller/ProductController/getAllProduct";
@@ -10,15 +7,6 @@ const app: Express = express();
 const port = 3030;
 
 app.use(cors());
-
-// Read SSL certificate and key
-const key = fs.readFileSync(path.join(__dirname, "../key.pem"));
-const cert = fs.readFileSync(path.join(__dirname, "../cert.pem"));
-
-const options = {
-  key,
-  cert,
-};
 
 // Middleware to parse JSON bodies
 app.use(express.json());
@@ -38,6 +26,3 @@ app.listen(port, () => {
   console.log(`Server is running on http://localhost:${port}`);
 });
 
-// https.createServer(options, app).listen(port, () => {
-//   console.log(`Server is running on https://localhost:${port}`);
-// });
