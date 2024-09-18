794b85a73ba54f442ee9c0eb96847151700246c4
repirisyan <repirisyan@icloud.com>
2024-09-18 14:05:04 +0700

    fix .env

--- src/db/connection.ts
+++ src/db/connection.ts
@@ -5,7 +5,7 @@ import dotenv from "dotenv";
 dotenv.config();
 
 // Build the MongoDB URI
-const mongoUri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DB}?authSource=${process.env.MONGO_AUTH_SOURCE}`;
+const mongoUri = `mongodb://${process.env.MONGO_USER}:${process.env.MONGO_PASSWORD}@${process.env.MONGO_HOST}:${process.env.MONGO_PORT}/${process.env.MONGO_DATABASE}?authSource=${process.env.MONGO_AUTH_SOURCE}`;
 
 const connectDB = async () => {
   try {
